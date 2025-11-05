fdbc9edf03aaf8fc57ed14403f6704189e28d7ff
mohamed aasif <153337575+AasifSweGit2017@users.noreply.github.com>
2025-11-05 18:38:46 +0000

    new update from aasifvip.info

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Card } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 
 
